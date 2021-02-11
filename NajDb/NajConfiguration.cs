79efc44096658990d0eba9d9d62857cea7b2a595
David Leblanc <lblancdavid@gmail.com>
2021-02-11 13:59:14 -0800

    Initial configuration and setup of the code

---
+++ NajDb/NajConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace NajDb
+{
+    public class NajConfiguration
+    {
+        public string ConnectionString { get; private set; }
+
+        public NajConfiguration(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+    }
+}
---
+++ NajDb/NajTable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NajDb
+{
+    public class NajTable<T> where T : class
+    {
+
+    }
+}
